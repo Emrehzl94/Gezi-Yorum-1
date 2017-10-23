2b8ea36e837c9daf6d709f1a149cd92ece2c309c
Murat Baki Yücel <bakiyucel38@gmail.com>
2017-10-24 01:48:44 +0300

    Map activity reorganized. Sanacbar messages created.

--- app/src/main/java/com/example/murat/gezi_yorum/TimeLine.java
+++ app/src/main/java/com/example/murat/gezi_yorum/TimeLine.java
@@ -1,67 +1,143 @@
 package com.example.murat.gezi_yorum;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 
-/**
- * Created by murat on 10/22/17.
- */
+public class exActivity extends Fragment implements OnMapReadyCallback, OnStreetViewPanoramaReadyCallback {
 
-public class TimeLine extends Fragment implements OnMapReadyCallback {
-    private GoogleMap mMap;
+    private GoogleMap map;
+    private GoogleMapsBottomSheetBehavior behavior;
+    private View parallax;
     private final static int MAP_PERMISSION_REQUEST = 1;
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle(getString(R.string.timeline));
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        SupportStreetViewPanoramaFragment streetViewPanoramaFragment = (SupportStreetViewPanoramaFragment) getChildFragmentManager()
+                .findFragmentById(R.id.parallax);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
+
+        final View bottomsheet = view.findViewById(R.id.bottomsheet);
+        behavior = GoogleMapsBottomSheetBehavior.from(bottomsheet);
+        parallax = view.findViewById(R.id.parallax);
+        behavior.setParallax(parallax);
+        behavior.setPeekHeight(300);
+        behavior.setState(GoogleMapsBottomSheetBehavior.STATE_COLLAPSED);
+        // wait for the bottomsheet to be laid out
+        bottomsheet.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                // set the height of the parallax to fill the gap between the anchor and the top of the screen
+                CoordinatorLayout.LayoutParams layoutParams = new CoordinatorLayout.LayoutParams(parallax.getMeasuredWidth(), behavior.getAnchorOffset() / 2);
+                parallax.setLayoutParams(layoutParams);
+                bottomsheet.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+
+        behavior.setBottomSheetCallback(new GoogleMapsBottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, @GoogleMapsBottomSheetBehavior.State int newState) {
+                // each time the bottomsheet changes position, animate the camera to keep the pin in view
+                // normally this would be a little more complex (getting the pin location and such),
+                // but for the purpose of an example this is enough to show how to stay centered on a pin
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.timeline_fragment, container, false);
+        return inflater.inflate(R.layout.activity_maps, container, false);
     }
 
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        this.map = googleMap;
+        behavior.anchorMap(map);
         if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},MAP_PERMISSION_REQUEST);
             return;
         }
-        mMap.setMyLocationEnabled(true);
+        map.setMyLocationEnabled(true);
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setCostAllowed(true);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        String provider = locationManager.getBestProvider(criteria, true);
+        Location lastKnownLocation = locationManager.getLastKnownLocation(provider);
+        if(lastKnownLocation != null) {
+            LatLng lastKnownLatLng = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(lastKnownLatLng,13.0f));
+        }
+        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                behavior.setState(GoogleMapsBottomSheetBehavior.STATE_COLLAPSED);
+                behavior.setHideable(false);
+                map.animateCamera(CameraUpdateFactory.newLatLng(marker.getPosition()));
+                return true;
+            }
+        });
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                behavior.setHideable(true);
+                behavior.setState(GoogleMapsBottomSheetBehavior.STATE_HIDDEN);
+            }
+        });
     }
+
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        boolean granted = false;
-        switch (requestCode){
-            case MAP_PERMISSION_REQUEST:
-                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    granted = true;
-                }else {
-                    Snackbar.make(getView(),"Bu işlev konum izni olmadan çalışmaz",Snackbar.LENGTH_LONG).show();
-                }
-                break;
-        }
-        if(granted){
-            ((MainActivity) getActivity()).changeFragment(new TimeLine());
-        }
+    public void onStreetViewPanoramaReady(StreetViewPanorama streetViewPanorama) {
+        //streetViewPanorama.setPosition(SYDNEY);
+        //streetViewPanorama.setUserNavigationEnabled(false);
     }
 }
