148febf885f86c63b65e3c87bc4407bff77db1e8
Murat Baki Yücel <bakiyucel38@gmail.com>
2018-01-10 02:40:06 +0300

    Önbellek ayarı hatalı. bu satırın olmaması lazım.

--- app/src/main/java/com/example/murat/gezi_yorum/Fragments/WebViewFragment.java
+++ app/src/main/java/com/example/murat/gezi_yorum/Fragments/WebViewFragment.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -42,7 +43,7 @@ public class WebViewFragment extends Fragment {
     private WebChromeClient client;
     @SuppressLint("SetJavaScriptEnabled")
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         progressBar = view.findViewById(R.id.progressBar);
@@ -138,7 +139,6 @@ public class WebViewFragment extends Fragment {
         });
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
-        settings.setAppCacheEnabled(true);
         webView.loadUrl(url);
 
         urlStack = new Stack<>();
